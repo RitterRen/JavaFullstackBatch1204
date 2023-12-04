0fc609e5fcbbb2d145b831c14db5d4ef9a7220da
Ritter Ren <renyaokun0907@gmail.com>
2023-12-04 12:52:40 -0800

    Day1 Assignment

---
+++ src/Day1/HelloWord.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World" + ". My name is Ritter and I really like playing basketball.");
+    }
+}
